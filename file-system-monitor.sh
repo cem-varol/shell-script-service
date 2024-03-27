859bc778e3f4a40042e3eaf251daee3409f962cd
xsantq <37873784+xsantq@users.noreply.github.com>
2024-03-27 15:08:32 +0300

    Update file-system-monitor.sh

--- file-system-monitor.sh
+++ file-system-monitor.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# Script check filesystem utilization every 120 seconds store in a file
+
+##########################################
+#Author Cem VAROL
+#Description: Check filesystem utilization every 120 seconds and log in a file
+
 while true
 do
 date >> /var/log/file-system-monitor.txt
